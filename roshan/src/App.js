7c9fe544d1c2e1d25ab133bbaef1964e7f6fe21c
roshanba <roshantravis@gmail.com>
2019-09-19 13:49:31 +0300

    created project with create react app and added files for hompeage

---
+++ roshan/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage.component'
+
+function App() {
+  return (
+    <Homepage/>
+  );
+}
+
+export default App;
